1c2635cfd087722d909df7a0dce5b74145f0ebaf
Yogesh-Ghatode <yogeshghatode09@gmail.com>
2022-07-17 19:01:16 +0530

    modify text in all files & addding background image

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,8 +2,8 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'cluster-map',
     style: 'mapbox://styles/mapbox/light-v10',
-    center: [-103.59179687498357, 40.66995747013945],
-    zoom: 3
+    center: [82.7792201,21.1289956],
+    zoom: 4
 });
 
 map.addControl(new mapboxgl.NavigationControl());
