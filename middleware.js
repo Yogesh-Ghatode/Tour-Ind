82f34a5d93e99229bb0ec5b4ec245eb9179b01b4
Yogesh-Ghatode <yogeshghatode09@gmail.com>
2022-08-11 14:51:05 +0530

    updated delete functionality for admin

--- middleware.js
+++ middleware.js
@@ -2,6 +2,7 @@ const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
 const Review = require('./models/review');
+let admin_id = "62f4c3b028b4de001614ef14" ;
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
@@ -26,7 +27,7 @@ module.exports.validateCampground = (req, res, next) => {
 module.exports.isAuthor = async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
-    if (!campground.author.equals(req.user._id)) {
+    if (!(campground.author.equals(req.user._id) || admin_id  == req.user._id ) ) {
         req.flash('error', 'You do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
     }
@@ -36,7 +37,7 @@ module.exports.isAuthor = async (req, res, next) => {
 module.exports.isReviewAuthor = async (req, res, next) => {
     const { id, reviewId } = req.params;
     const review = await Review.findById(reviewId);
-    if (!review.author.equals(req.user._id)) {
+    if (!(review.author.equals(req.user._id) ||  admin_id == req.user._id )) {
         req.flash('error', 'You do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
     }
